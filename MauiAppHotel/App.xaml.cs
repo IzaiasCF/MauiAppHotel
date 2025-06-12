e9bfa98344b0537239a09c23a0a29e189fa76e9f
IzaiasCF <izaiascorreafilho@gmail.com>
2025-06-12 09:36:12 -0300

    add commit

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -7,7 +7,8 @@ namespace MauiAppHotel
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
