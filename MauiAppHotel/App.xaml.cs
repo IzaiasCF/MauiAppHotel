e06997a86277d737ec46c03981d74bb123c3444f
IzaiasCF <izaiascorreafilho@gmail.com>
2025-06-13 13:04:29 -0300

    add comit

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,41 @@
-﻿
+﻿using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte SuperLuxo",
+                ValorDiariaAdulto = 110.00,
+                ValorDiariaCrianca = 55.00
+             },
+
+             new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.00,
+                ValorDiariaCrianca = 40.00
+             },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.00,
+                ValorDiariaCrianca = 25.00
+             },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Básica",
+                ValorDiariaAdulto = 25.00,
+                ValorDiariaCrianca = 12.50
+             }
+        };
+
         public App()
         {
             InitializeComponent();
