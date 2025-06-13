d6e988bced128913b75417164e6740051e3f6396
IzaiasCF <izaiascorreafilho@gmail.com>
2025-06-13 13:42:51 -0300

    add commit

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
@@ -20,16 +22,18 @@ public partial class ContratacaoHospedagem : ContentPage
         dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
         try
         {
-            Navigation.PushAsync(new HospedagemContratada());
+            Hospedagem h = new Hospedagem();
+
+            await Navigation.PushAsync(new HospedagemContratada());
 
         }
         catch (Exception ex)
         {
-            DisplayAlert("Ops!", ex.Message, "Ok");
+            await DisplayAlert("Ops!", ex.Message, "Ok");
         }
     }
 
