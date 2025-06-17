b6d340a982c5d1e98e32dec9b7b2721355faae35
IzaiasCF <izaiascorreafilho@gmail.com>
2025-06-17 11:08:20 -0300

    add commit

--- MauiAppHotel/SobrePage.xaml.cs
+++ MauiAppHotel/SobrePage.xaml.cs
@@ -2,15 +2,17 @@ namespace MauiAppHotel;
 
 public partial class SobrePage : ContentPage
 {
-	public SobrePage()
-	{
-		InitializeComponent();
+    public SobrePage()
+    {
+        InitializeComponent();
 
+        // Forçar ocultar NavigationBar via C#
         NavigationPage.SetHasNavigationBar(this, false);
     }
 
     private async void Button_Clicked(object sender, EventArgs e)
     {
+        // Voltar para a página anterior
         await Navigation.PopAsync();
     }
 }
--- MauiAppHotel/Views/HospedagemContratada.xaml.cs
+++ MauiAppHotel/Views/HospedagemContratada.xaml.cs
@@ -2,21 +2,23 @@ namespace MauiAppHotel.Views;
 
 public partial class HospedagemContratada : ContentPage
 {
-	public HospedagemContratada()
-	{
-		InitializeComponent();
-	}
+    public HospedagemContratada()
+    {
+        InitializeComponent();
+
+        // Isso garante que o cabeçalho não aparece ao abrir a página
+        NavigationPage.SetHasNavigationBar(this, false);
+    }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-		try
-		{
-
-			Navigation.PopAsync();
-
-		} catch (Exception ex) {
-
-			DisplayAlert("Ops!", ex.Message, "Ok");
-		}
+        try
+        {
+            Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops!", ex.Message, "Ok");
+        }
     }
 }
